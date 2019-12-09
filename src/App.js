c336c50b697c494a55e23c79b392c95ba0468020
Ismael REPIR <repirismael6@gmail.com>
2019-12-10 00:50:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Counter() {
+    // Déclare une nouvelle variable d'état, qu’on va appeler « count »
+    // Déclare une fonction setCount qui permet de modifier la valeur de count
+    const [count, setCount] = useState(0);
+
+    function handleChange(event) {
+        setCount({ value: event.target.value });
+    };
+
+    return (
+        <div>
+            <p> Le compteur est à:
+                <input type="number" value={count} onChange={handleChange} />
+            </p>
+            <button onClick={() => setCount(count + 1)}> +1 </button>
+            <button onClick={() => setCount(count - 1)}> -1 </button>
+        </div>
+    );
 }
 
-export default App;
+export default Counter;
+
+// Avec les classes:
+
+// import React, { Component } from 'react';
+
+// class Counter extends Component {
+
+//   constructor() {
+//     super();
+//     this.state = {
+//       count: 0,
+//     };
+//   }
+
+//   handleCounter = (value) => {
+//     this.setState({count: value});
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p>Le compteur est  à : {this.state.count}</p>
+//         <button  onClick={() =>  this.handleCounter(this.state.count + 1)}>
+//           +1
+//         </button>
+//         <button  onClick={() =>  this.handleCounter(this.state.count - 1)}>
+//           -1
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Counter;
